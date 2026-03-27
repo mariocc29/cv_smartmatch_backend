16d501fae035b1598106b1edea45eb68f0d4ae91
Mario Contreras <mariocc29@gmail.com>
2026-03-27 12:45:49 -0400

    setting as a env var ai model

--- app/initializers/generative_ai.py
+++ app/initializers/generative_ai.py
@@ -2,3 +2,4 @@ import os
 
 GENERATIVE_AI_ENGINE = 'google_generativeai'
 GENERATIVE_AI_API_KEY = os.getenv('GENERATIVE_AI_API_KEY')
+GENERATIVE_AI_MODEL = 'gemini-3-flash-preview'
--- common/services/generative_ai/google_generative_ai_repository.py
+++ common/services/generative_ai/google_generative_ai_repository.py
@@ -1,11 +1,11 @@
 import google.generativeai as genai
 
-from app.initializers.generative_ai import GENERATIVE_AI_API_KEY
+from app.initializers.generative_ai import GENERATIVE_AI_API_KEY, GENERATIVE_AI_MODEL
 from common.services.generative_ai.generative_ai_repository import GenerativeAIRepository
 
 class GoogleGenerativeAIRepository(GenerativeAIRepository):
   def send(self, prompt: str) -> str:
     genai.configure(api_key=GENERATIVE_AI_API_KEY)
-    model = genai.GenerativeModel('gemini-3-flash-preview')
+    model = genai.GenerativeModel(GENERATIVE_AI_MODEL)
     response = model.generate_content(prompt)
     return response.text
